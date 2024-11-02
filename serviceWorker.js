6b6f229466d02edfd19c48bd0035eb4324fafcc0
Md. Shaidur Rahaman Tanzid <mdshaidurrahamantanzid@gmail.com>
2024-11-02 17:14:42 +0600

    Pwa application

---
+++ serviceWorker.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', event => {
+    event.waitUntil(
+      caches.open('my-cache').then(cache => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/styles.css',
+          '/script.js',
+          'images/image3.jpg',
+          'images/image3.jpg'
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', event => {
+    event.respondWith(
+      caches.match(event.request).then(response => {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
